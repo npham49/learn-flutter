a9649f533e7480e46151526aefaf70da07bf715b
npham49 <brian.d.pham@gov.bc.ca>
2024-02-19 13:25:59 -0800

    added TODO scaffolding for lesson

---
+++ todo_provider/lib/models/todo_model.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:equatable/equatable.dart';
+import 'package:uuid/uuid.dart';
+
+Uuid uuid = const Uuid();
+
+class Todo with EquatableMixin {
+  final String id;
+  final String desc;
+  final bool completed;
+
+  Todo({String? id, required this.desc, this.completed = false}): id = id ?? uuid.v4();
+
+  @override
+  List<Object> get props => [id, desc, completed];
+}
